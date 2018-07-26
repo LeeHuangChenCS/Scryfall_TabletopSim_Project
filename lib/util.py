08f647a3a50202190811355cfb0f8a833d4b4db3
LeeHuangChen <lhc1@rice.edu>
2018-07-26 18:34:50 -0500

    fixed a bug with the splitting of file urls

--- lib/util.py
+++ lib/util.py
@@ -2,13 +2,20 @@ import os
 
 # generate all the directories needed for the given path (helper function)
 def generateDirectories(path):
-    folders = path.split("/")
+    #folders = path.split("/")
+    path = os.path.normpath(path)
+    folders = path.split(os.sep)
+    #folders = os.path.split(path)
+    print(folders)
+
     curdir = ""
     prevFolder = ""
     for folder in folders:
-        prevFolder = curdir
-        curdir = os.path.join(curdir, folder)
-        if not os.path.exists(curdir):
-            # print curdir
-            # os.chmod(prevFolder,stat.S_IWRITE)
-            os.mkdir(curdir)
+        print(folder)
+        if len(folder.strip())>0:
+            prevFolder = curdir
+            curdir = os.path.join(curdir, folder)
+            if not os.path.exists(curdir):
+                # print curdir
+                # os.chmod(prevFolder,stat.S_IWRITE)
+                os.mkdir(curdir)
